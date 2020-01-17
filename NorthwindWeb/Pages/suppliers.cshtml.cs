b542530a0d8ac10ae2ee914645c78acf8d5e325f
Zafarjon <swdev7@Zafarjons-MacBook-Pro.local>
2020-01-17 22:58:32 +0500

    Configuring EF Core as a service

--- NorthwindWeb/Pages/suppliers.cshtml.cs
+++ NorthwindWeb/Pages/suppliers.cshtml.cs
@@ -1,20 +1,25 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Collections.Generic;
+using System.Linq;
+using Packt.Shared;
 
 namespace NorthwindWeb.Pages
 {
     public class SupplierModel : PageModel
     {
+        private Northwind db;
+
+        public SupplierModel(Northwind injectedContext)
+        {
+            db = injectedContext;
+        }
         public IEnumerable<string> Suppliers { get; set; }
 
         public void OnGet()
         {
             ViewData["Title"] = "Northwind Web Site - Suppliers.";
 
-            Suppliers = new[] 
-            {
-                "Alpha Co.", "Betta Limited.", "Gamma Corp."
-            };
+            Suppliers = db.Suppliers.Select(s => s.CompanyName);
         }
     }
 }
--- NorthwindWeb/Startup.cs
+++ NorthwindWeb/Startup.cs
@@ -1,12 +1,15 @@
 using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Packt.Shared;
 
 namespace NorthwindWeb
 {
@@ -16,6 +19,9 @@ namespace NorthwindWeb
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            var databasePath = Path.Combine("..", "Northwind.db");
+            services.AddDbContext<Northwind>(options => 
+                options.UseSqlite($"Data Source={databasePath}"));
             services.AddRazorPages();
         }
 
