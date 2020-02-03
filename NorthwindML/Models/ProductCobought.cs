6a966651f5517006f24c4324ccd4b9d50834d7b0
swdev77 <swdev77@gmail.com>
2020-02-03 17:49:16 +0500

    Created a ML website project, the data and view models

---
+++ NorthwindML/Models/ProductCobought.cs
@@ -0,0 +1,13 @@
+using Microsoft.ML.Data;
+
+namespace NorthwindML.Models
+{
+    public class ProductCobought
+    {
+        [KeyType(77)]
+        public uint ProductID { get; set; }
+
+        [KeyType(77)]
+        public uint CoboughtProductID { get; set; }
+    }
+}
